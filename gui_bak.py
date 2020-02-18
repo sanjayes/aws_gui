c107397540a79e189e22ed17e184dc8408e8d29b
sanjayes <samme63442@gmail.com>
2020-02-18 14:16:56 +0530

    Tue Feb 18 14:16:39 IST 2020

--- gui_bak.py
+++ gui_bak.py
@@ -1,5 +1,5 @@
 import sys
-from tkinter import Label, Button, Tk, Listbox, END, Frame, LEFT, BOTTOM, simpledialog
+from tkinter import Label, Button, Tk, Listbox, END, Frame, LEFT, simpledialog, messagebox, BOTTOM
 import boto3
 import threading
 
@@ -13,60 +13,59 @@ label = Label(root)
 label.pack()
 
 listbox = Listbox(root, width=85)
-listbox.pack()
+listbox.pack(side=LEFT)
+
+statebox = Listbox(root)
+statebox.pack(side=LEFT)
 
 
 def create():
     img_id = simpledialog.askstring("Instance Details", "Image ID (ami-0123b531fc646552f):")
-    if img_id is None:
-        sys.exit()
-    # else:
-    #     img_id = "ami-0123b531fc646552f"
+    # if img_id is None:
+    #     breakpoint(None)
     inst_type = simpledialog.askstring("Instance Details", "Instance type (t2.micro):")
-    if inst_type is ' ':
-        inst_type = "t2.micro"
+    # if inst_type is ' ':
+    #     inst_type = "t2.micro"
     key = simpledialog.askstring("Instance Details", "Key:")
-    if key is ' ':
-        key = "key"
+    # if key is ' ':
+    #     key = "key"
     tag = simpledialog.askstring("Instance Details", "Tag:")
-    if tag is ' ':
-        tag = " "
+    # if tag is ' ':
+    #     tag = " "
     s_g = simpledialog.askstring("Instance Details", "Security Group (default):")
-    if s_g is ' ':
-        s_g = "default"
+    # if s_g is ' ':
+    #     s_g = "default"
     c = simpledialog.askinteger("Instance Details", "Max Count (1):", initialvalue=1)
-    if c is ' ':
-        c = 1
+    # if c is ' ':
+    #     c = 1
+    if img_id is None or inst_type is None or key is None or c is None:
+        messagebox.showwarning("Error", "Incomplete Information")
+        breakpoint(None)
 
-    print(img_id)
-    print(inst_type)
-    print(key)
-    print(tag)
-    print(s_g)
-    print(c)
+    ec2 = boto3.resource('ec2')
+    ec2.create_instances(
+        ImageId=img_id,
+        InstanceType=inst_type,
+        KeyName=key,
+        TagSpecifications=(
+            {
+                'ResourceType': 'instance',
+                'Tags': [
+                    {
+                        'Key': 'Name',
+                        'Value': tag
+                    },
+                ]
+            },
+        ),
+        SecurityGroups=[
+            s_g,
+        ],
+        MinCount=1,
+        MaxCount=c
+    )
 
-    # ec2 = boto3.resource('ec2')
-    # ec2.create_instances(
-    #     ImageId=img_id,
-    #     InstanceType=inst_type,
-    #     KeyName=key,
-    #     TagSpecifications=(
-    #         {
-    #             'ResourceType': 'instance',
-    #             'Tags': [
-    #                 {
-    #                     'Key': 'Name',
-    #                     'Value': tag
-    #                 },
-    #             ]
-    #         },
-    #     ),
-    #     SecurityGroups=[
-    #         s_g,
-    #     ],
-    #     MinCount=1,
-    #     MaxCount=c
-    # )
+    messagebox.showinfo("Information", "Instance Created Successful")
 
 
 def describe():
@@ -76,7 +75,7 @@ def describe():
     # print(ec2.describe_instances()['Reservations'][0]['Instances'][0]['SecurityGroups'][0]['GroupId'])
     # print(ec2.describe_instances()['Reservations'][0]['Instances'][0]['State']['Name'])
     label.config(text=(
-        "|         Instance ID         |      Public IP      |    State    |                                        Public DNS                                 |"))
+        "         Instance ID         |      Public IP      |                                        Public DNS                                 |    State    "))
     for a in ec2.describe_instances()['Reservations']:
         # pprint.pprint(a)
         for b in a['Instances']:
@@ -84,19 +83,41 @@ def describe():
             for c in b['NetworkInterfaces']:
                 # pprint.pprint(c)
                 try:
-                    listbox.insert(END, "| " + b['InstanceId'] + " | " + b['PublicIpAddress'] + "|  " + b['State'][
-                        'Name'] + " | " + c['Association']['PublicDnsName'] + " |")
+                    # listbox.insert(END, "| " + b['InstanceId'] + " | " + b['PublicIpAddress'] + "|  " + b['State'][
+                    #     'Name'] + " | " + c['Association']['PublicDnsName'] + " |")
+                    listbox.insert(END,
+                                   "   " + b['InstanceId'] + "   |   " + b['PublicIpAddress'] + "    |    " +
+                                   c['Association'][
+                                       'PublicDnsName'])
                 except Exception:
-                    listbox.insert(END, "| " + b['InstanceId'] + " | -------------- |  " + b['State']['Name'] +
-                                   " | -------------------------------------------------- |")
-    threading.Timer(5.0, describe).start()
+                    # listbox.insert(END, "| " + b['InstanceId'] + " | -------------- |  " + b['State']['Name'] +
+                    #                " | -------------------------------------------------- |")
+                    listbox.insert(END, b[
+                        'InstanceId'] + " | -------------- | -------------------------------------------------- ")
+
+    def state():
+        for d in ec2.describe_instances()['Reservations']:
+            # pprint.pprint(a)
+            for e in d['Instances']:
+                # pprint.pprint(b)
+                for f in e['NetworkInterfaces']:
+                    # pprint.pprint(c)
+                    statebox.insert(END, "      " + e['State']['Name'])
+
+    t = threading.Timer(2.0, state)
+    t.setDaemon(True)
+    t.start()
 
 
-desc = Button(frame, text="Instances", command=describe)
-cre = Button(frame, text="Create", command=create)
+def close():
+    sys.exit()
 
-desc.pack(side=LEFT)
-cre.pack(side=LEFT)
+
+close = Button(root, text="x", command=close)
+cre = Button(root, text="Create", command=create)
+
+close.pack()
+cre.pack()
 
 describe()
 
